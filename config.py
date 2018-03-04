f84d34f6b9b726e1dc677dc24c7da10cf3b81e5f
mrtiotio <mrtiotio1@gmail.com>
2018-03-04 20:34:25 +0100

    03-04-2018-

---
+++ config.py
@@ -0,0 +1,9 @@
+#   Author Mrtiotio
+#   This is the configuration file. 
+#   Please make sure no bot-key's are synced to github.
+#   Change to values to your liking. 
+
+#   The bot token. Get a new token from the telegram Botfather. 
+token = "<your token>"
+
+
