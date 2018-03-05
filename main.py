2ccc5683a6d32609e7ca884bc7f54173fa51b74e
mrtiotio <mrtiotio1@gmail.com>
2018-03-05 09:35:41 +0100

    Minor changes

--- main.py
+++ main.py
@@ -3,36 +3,7 @@ import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-
-def start(bot, update):
-    keyboard =  [[InlineKeyboardButton("Option 1", callback_data='1')],
-                [InlineKeyboardButton("Option 2", callback_data='2')],
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-def button(bot, update):
-    query = update.callback_query
-
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
-
-
-def help(bot, update):
-    update.message.reply_text("Use /start to test this bot.")
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
